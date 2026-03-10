dae61d4ac38983de98b8c012ed21008bcfb51b3b
XiaoniXu <xx2485@cumc.columbia.edu>
2026-03-09 21:04:00 -0400

    Organized reports into subfolder, removed bold fonts, and included PDF versions.

--- render_report_20260223.R
+++ render_report_20260223.R
@@ -1,12 +1,4 @@
 
 library(rmarkdown)
-
-# Paths
-input_file <- "c:/Users/berns/Desktop/Research/Valeri/Codes/med_longitudinal_command/20260223_Progress_Report_XX.Rmd"
-output_pdf <- "c:/Users/berns/Desktop/Research/Valeri/Codes/med_longitudinal_command/20260223_Progress_Report_XX.pdf"
-
-# Set Pandoc path (from render_pdf.R)
 Sys.setenv(RSTUDIO_PANDOC = "C:/Program Files/RStudio/resources/app/bin/quarto/bin/tools")
-
-# Render
-rmarkdown::render(input_file, output_format = "pdf_document", output_file = output_pdf)
+rmarkdown::render("reports/20260223_Progress_Report_XX.Rmd", output_format = "pdf_document")
--- render_report_20260309.R
+++ render_report_20260309.R
@@ -1,5 +1,8 @@
 
 library(rmarkdown)
 
+# Specify the path to Pandoc found on this system
+Sys.setenv(RSTUDIO_PANDOC = "C:/Program Files/RStudio/resources/app/bin/quarto/bin/tools")
+
 # Render the RMarkdown to PDF
-rmarkdown::render("20260309_Progress_Report_XX.Rmd", output_format = "pdf_document")
+rmarkdown::render("reports/20260309_Progress_Report_XX.Rmd", output_format = "pdf_document")
